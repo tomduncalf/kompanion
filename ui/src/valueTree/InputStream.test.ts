9be78fffb88b765907cdb973722570991b6d8ba6
Tom Duncalf <tom@tomduncalf.com>
2021-05-09 16:41:58 +0100

    Change InputStream internal format to Uint8Array, add readCompressedInt

--- ui/src/valueTree/InputStream.test.ts
+++ ui/src/valueTree/InputStream.test.ts
@@ -1,13 +1,44 @@
 import { InputStream } from "./InputStream";
+import { TextEncoder } from "util";
 
-test("readString", () => {
-  const stream = new InputStream(
-    "1test1" + String.fromCharCode(0) + "2test2" + String.fromCharCode(0)
-  );
+describe("readString", () => {
+  test("read multiple strings", () => {
+    const encoder = new TextEncoder();
+    const stream = new InputStream(
+      encoder.encode(
+        "1test1" + String.fromCharCode(0) + "2test2" + String.fromCharCode(0)
+      )
+    );
 
-  expect(stream.readString()).toEqual("1test1");
-  expect(stream.readString()).toEqual("2test2");
-  expect(stream.readString()).toEqual("");
+    expect(stream.readString()).toEqual("1test1");
+    expect(stream.readString()).toEqual("2test2");
+    expect(stream.readString()).toEqual("");
+  });
 });
 
-test("readCompressedInt", () => {});
+// Using compressedInt values written to a file in JUCE
+describe("readCompressedInt", () => {
+  test("1 byte", () => {
+    const compressed = [0x01, 0x7b];
+    const stream = new InputStream(Uint8Array.from(compressed));
+    expect(stream.readCompressedInt()).toEqual(123);
+  });
+
+  test("2 bytes", () => {
+    const compressed = [0x02, 0xd2, 0x04];
+    const stream = new InputStream(Uint8Array.from(compressed));
+    expect(stream.readCompressedInt()).toEqual(1234);
+  });
+
+  test("3 bytes", () => {
+    const compressed = [0x03, 0x87, 0xd6, 0x12];
+    const stream = new InputStream(Uint8Array.from(compressed));
+    expect(stream.readCompressedInt()).toEqual(1234567);
+  });
+
+  test("4 bytes", () => {
+    const compressed = [0x04, 0x15, 0xcd, 0x5b, 0x07];
+    const stream = new InputStream(Uint8Array.from(compressed));
+    expect(stream.readCompressedInt()).toEqual(123456789);
+  });
+});
--- ui/src/valueTree/InputStream.ts
+++ ui/src/valueTree/InputStream.ts
@@ -1,5 +1,7 @@
+import { assert } from "console";
+
 export class InputStream {
-  constructor(private data: string) {}
+  constructor(private data: Uint8Array) {}
 
   private readPosition = 0;
 
@@ -8,21 +10,42 @@ export class InputStream {
 
     while (true) {
       const c = this.readByte();
+      if (c === 0) return buffer;
 
-      if (c.charCodeAt(0) === 0) return buffer;
-
-      buffer += String(c);
+      buffer += String.fromCharCode(c);
     }
   };
 
-  readByte = (): string => {
+  readByte = (): number => {
     const byte = this.data[this.readPosition];
     this.incrementReadPosition(1);
     return byte;
   };
 
   readCompressedInt = (): number => {
-    // const
+    const sizeByte = this.readByte();
+
+    if (sizeByte === 0) return 0;
+
+    const numBytes = sizeByte & 0x7f;
+
+    if (numBytes > 4) {
+      assert(false);
+      return 0;
+    }
+
+    let bytes: number[] = [0, 0, 0, 0];
+
+    // TODO error handling: if (read (bytes, numBytes) != numBytes)
+    for (let i = 0; i < numBytes; i++) {
+      bytes[i] = this.readByte();
+    }
+
+    // from ByteOrder::makeInt
+    const number =
+      bytes[0] | (bytes[1] << 8) | (bytes[2] << 16) | (bytes[3] << 24);
+
+    return sizeByte >> 7 ? -number : number;
   };
 
   private incrementReadPosition = (delta: number) => {
@@ -36,7 +59,7 @@ export class InputStream {
   //   while (size > 0)
   //   {
   //       const numToRead = Math.min(size, 0x70000000);
-  //       const numRead = read (juce::addBytesToPointer (destBuffer, totalRead), numToRead);
+  //       const numRead = this.read (juce::addBytesToPointer (destBuffer, totalRead), numToRead);
   //       jassert (numRead <= numToRead);
 
   //       if (numRead < 0) return (ssize_t) numRead;
